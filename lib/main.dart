cb902f4c1c04ce5a486c890eb1e5f983059ab566
x-io Technologies <social@x-io.co.uk>
2024-06-11 10:43:15 +0100

    Reduce resolution to 1 ms

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   RawDatagramSocket.bind(InternetAddress.anyIPv4, 9000).then((RawDatagramSocket udpSocket) {
     udpSocket.broadcastEnabled = true;
     Timer.periodic(const Duration(seconds: 1), (timer) {
-      final String note = '{"note":"${DateFormat("yyyy-MM-dd HH:mm:ss.SSSS").format(DateTime.now()).toString()}"}\n';
+      final String note = '{"note":"${DateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(DateTime.now()).toString()}"}\n';
       const String blink = '{"blink":null}\n';
       udpSocket.send(utf8.encode(note + blink), InternetAddress("255.255.255.255"), 9000);
     });
